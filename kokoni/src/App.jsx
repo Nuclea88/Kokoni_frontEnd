105f5916571c9a0309e433e3aebff390519c2a4b
Nuclea88 <ingrid.lopov@gmail.com>
2026-03-23 12:45:12 +0100

    feat: register page added

--- kokoni/src/App.jsx
+++ kokoni/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
 import { AuthProvider, AuthContext } from './context/AuthContext';
 import { useContext } from 'react';
 import Login from './pages/Login';
+import Register from './pages/Register';
+import DashboardLayout from './components/templates/DashboardLayout';
 // Componente para proteger rutas (Dashboard, etc)
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated, loading } = useContext(AuthContext);
@@ -21,7 +23,25 @@ function AppRoutes() {
     <Routes>
       <Route path="/" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<PrivateRoute><DashboardMock /></PrivateRoute>} />
+      <Route path="/register" element={<Register />} />
+      
+      {/* 
+        Si está autenticado, el usuario entra al layout del Dashboard (con las barras superior e inferior).
+        Todo lo que haya DENTRO de estas rutas se pintará entre ambas barras.
+      */}
+      <Route path="/dashboard" element={<PrivateRoute><DashboardLayout /></PrivateRoute>}>
+         <Route index element={
+           <div className="animate-fade-in-up">
+              <h2 className="text-textMuted text-xs font-bold tracking-widest mb-4">CONTINÚA LEYENDO</h2>
+              <div className="p-8 text-center glass-panel">
+                <p>Aquí pondremos tus mangas leyendo próximamente.</p>
+              </div>
+           </div>
+         } />
+         <Route path="novedad" element={<div className="text-center mt-10 animate-fade-in-up text-primary">Pronto: Sección de Novedades</div>} />
+         <Route path="explorar" element={<div className="text-center mt-10 animate-fade-in-up text-secondary">Pronto: Explorador y Búsqueda</div>} />
+         <Route path="ajustes" element={<div className="text-center mt-10 animate-fade-in-up">Pronto: Ajustes de Tema</div>} />
+      </Route>
     </Routes>
   );
 }
